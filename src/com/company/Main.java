abe70d4cb44728a424c36547250578205dd9c94d
Felipe Steffan <felip.developer@hotmail.com>
2022-07-11 10:41:33 -0300

    Merge branch 'main' of github.com:fehsteffan/curso-programacao

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,24 @@
 package com.company;
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Main {
     public static void main (String[] args){
 
-    Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-    int x = sc.nextInt();
+        double largura = sc.nextDouble();
+        double comprimento = sc.nextDouble();
+        double metroQuadrado = sc.nextDouble();
+        double area = largura * comprimento;
+        double preco = area * metroQuadrado;
 
-    int soma = 0;
-    while (x != 0){
-        soma = soma + x;
-        x = sc.nextInt();
-
-    }
-
-    System.out.println(soma);
-
+        System.out.printf("AREA = %.2f%n", area);
+        System.out.printf("PRECO = %.2f%n", preco);
 
 
-
-    sc.close();
+        sc.close();
 
     }
 }
